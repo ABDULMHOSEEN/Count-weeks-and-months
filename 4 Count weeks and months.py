65c33e8ed0936af830f4adad0cf87887d14585a9
ABDULMOHSEEN <aam20122003@gmail.com>
2021-07-25 15:40:06 +0300

    Add comments

--- 4 Count weeks and months.py
+++ 4 Count weeks and months.py
@@ -1,12 +1,16 @@
+# define a function that make seconds into other form
 def format_duration(seconds):
-    if seconds < 1: return "now"
-    time = [0, 0, 0, 0, 0]
+    # if the seconds is less than 1 that mean now
+    if seconds == 0: return "now"
+    elif seconds < 0: return "Error invalid number of seconds"
+    # make a list that will save the kinds of times
+    time = [0, 0, 0, 0, 0]  # years - months - hour - minutes - seconds
     time[0] = seconds // (365 * 24 * 60 * 60)
     time[1] = seconds % (365 * 24 * 60 * 60) // (24 * 60 * 60)
     time[2] = seconds % (365 * 24 * 60 * 60) % (24 * 60 * 60) // (60 * 60)
     time[3] = seconds % (365 * 24 * 60 * 60) % (24 * 60 * 60) % (60 * 60) // 60
     time[4] = seconds % (365 * 24 * 60 * 60) % (24 * 60 * 60) % (60 * 60) % 60
-
+    # make a string call out to git the format of final print
     out = ""
     for i in range(len(time)):
         if i == 0:
@@ -26,4 +30,5 @@ def format_duration(seconds):
                 out += " and "
             out += str(time[i]) + per + ("s" if time[i] > 1 else "")
     return out
-print(format_duration(400))
+seconds = int(input("Enter the namber of seconds: "))
+print(format_duration(seconds))
